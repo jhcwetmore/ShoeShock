a96e0233a7749f143bcd10dc61193c112aa16e91
Jon <jhcwetmore@phiretech.com>
2020-04-22 10:04:43 -0500

    Initial Commit

---
+++ ShoeShock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoeShock
+//
+//  Created by Jonathan Wetmore on 4/22/20.
+//  Copyright © 2020 Jonathan Wetmore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
